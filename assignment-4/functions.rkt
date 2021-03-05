1cdac44a7d7f5e3f596de4beb7ea106e474eca6d
Tarush Roy (Troy) <46633878+troy-csc@users.noreply.github.com>
2021-03-04 21:19:17 -0800

    Add files via upload
    Done

--- assignment-4/functions.rkt
+++ assignment-4/functions.rkt
@@ -1,42 +1,142 @@
-#lang racket
+; CSC 330
+; Programming Languages
+; Assignment 4
 
+; Student Information
+; Name: Tarush Roy
+; VNum: V00883469
+
+#lang racket
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
-; part 1
+
+; ---------- part 1 ---------- ;
 (define nat-num-stream
   (letrec
       ([f (lambda (x)
             (cons x (lambda () (f (+ x 1)))))])
     (lambda () (f 0))))
 
-;#1
-(define add-pointwise  #f)
+; #1: add-pointwise
+(define (add-pointwise ls1 ls2)
+  (if (list? ls1)     ; check both parameters
+      (if (list? ls2) ; for lists
+          (if (null? ls1)
+              ls2
+              (if (null? ls2)
+                  ls1
+                  (cons (+ (car ls1) (car ls2)) (add-pointwise (cdr ls1) (cdr ls2))))) ; both lists are non-empty here
+          (error "illegal parameter"))
+      (error "illegal parameter")))
+          
 
-;#2
-(define add-pointwise-lists #f)
+; #2: add-pointwise-lists
+(define (add-pointwise-lists ls)
+  (letrec (; helper function to sum list
+           [sum (lambda (xs) (if (null? xs) 0 (+ (car xs) (sum (cdr xs)))))]
+           ; get a list of heads of a list of lists
+           [get-heads (lambda (ls) (map (lambda (l) (if (null? l) 0 (car l))) ls))]
+           ; get a list of tails of a list of lists
+           [get-tails (lambda (ls) (map (lambda (l) (if (null? l) null (cdr l))) ls))]
+           ; checks list of lists for empty lists
+           [list-nulls? (lambda (ls) (if (null? ls) #t (if (null? (car ls)) (list-nulls? (cdr ls)) #f)))]
+           ; checks list of lists for illegal parameters
+           [list-lists? (lambda (ls) (if (null? ls) #t (if (list? (car ls)) (list-lists? (cdr ls)) #f)))])
+    (if (list? ls)
+        (if (null? ls)
+            null
+            (if (list-nulls? ls)
+                null
+                (if (list-lists? ls)
+                    (cons (sum (get-heads ls)) (add-pointwise-lists (get-tails ls))) ; sum heads, recurse of list of tails
+                    (error "illegal parameter"))))
+        (error "illegal parameter"))))
 
-;#3
-(define add-pointwise-lists-2 #f)
+; #3: add-pointwise-lists-2
+;     non-recursive
+(define (add-pointwise-lists-2 ls)
+  (if (list? ls)
+      (foldr add-pointwise null ls) ; simple fold call
+      (error "illegal parameter")))
 
-;#4
-(define stream-for-n-steps #f)
+; #4: stream-for-n-steps
+(define (stream-for-n-steps s n)
+  (letrec ([f (lambda (stream counter acc)
+                (if (= counter n)
+                    acc
+                    (let ([pr (stream)])
+                      (f (cdr pr) (+ counter 1) (append acc (list (car pr)))))))])
+    (f s 0 null)))
 
-;#5
-(define fibo-stream #f)
+; #5: fibo-stream
+(define fibo-stream
+  (letrec ([f (lambda (x1 x2)
+                (if (= x1 0)
+                    (cons x1 (lambda () (cons x2 (lambda () (cons x2 (lambda () (f x2 x2))))))) ; Manually inserting first exception values
+                    (cons (+ x1 x2) (lambda () (f x2 (+ x1 x2))))))])
+    (lambda () (f 0 1))))
 
-;#6
-(define filter-stream #f)
+; #6: filter-stream
+(define (filter-stream fn s)
+  (letrec ([f (lambda (stream)
+                (let ([pr (stream)])
+                  (if (fn (car pr))
+                      (cons (car pr) (lambda () (f (cdr pr))))
+                      (f (cdr pr)))))])
+    (lambda () (f s))))
 
-;#7
-(define palyndromic-numbers #f)
+; #7: palyndromic-numbers stream
+(define palyndromic-numbers
+  (letrec ([palindrome? (lambda (n)
+                          (equal? (string->list (~a n)) (reverse (string->list (~a n)))))])
+    (filter-stream palindrome? nat-num-stream)))
+                
+; #8: macro create-stream
+(define-syntax create-stream
+  (syntax-rules (using starting at with increment)
+    [(create-stream name using f starting at i0 with increment delta)
+     (define (name) ; define stream with name
+       (letrec ([fn (lambda (x)
+                     (cons (f x) (lambda () (fn (+ x delta)))))]) ; add f(x) and call with increment
+         (fn i0)))])) ; start with initial value
 
-;#8 macro create-stream
 
-; part 2
+; ---------- part 2 ---------- ;
 
-;#1
-(define vector-assoc #f)
+; #1: vector-assoc
+(define (vector-assoc v vec)
+  (if (= (vector-length vec) 0)
+      #f
+      (letrec ([f (lambda (pos)
+                    (if (= pos (vector-length vec)) ; if last item
+                        (if (pair? (vector-ref vec (- pos 1))) ; checks for pair
+                            (if (= (car (vector-ref vec (- pos 1))) v) ; check value for equal
+                                (vector-ref vec pos) ; return value
+                                #f) ; return false because nothing was found in the vector
+                            #f)
+                        (if (pair? (vector-ref vec pos)) ; not last item
+                            (if (= (car (vector-ref vec pos)) v)
+                                (vector-ref vec pos)
+                                (f (+ pos 1))) ; recursive call with next position accumulator
+                            (f (+ pos 1)))))]) ; recursive call
+        (f 0)))) ; call to helper function
 
-;#2
-(define cached-assoc #f)
+; #2: cached-assoc
+(define (cached-assoc xs n)
+  (letrec ([memo (make-vector n [(lambda () #f)])] ; cache initialized with #f
+           [pos 0] ; counter for current position in cache
+           [f (lambda (v)
+                (let ([ans (vector-assoc v memo)]) ; search cache for match
+                  (if ans
+                      ans ; return ans
+                      (let ([new-ans (assoc v xs)]) ; find match is list
+                        (if new-ans
+                            (begin
+                              (vector-set! memo pos new-ans) ; insert pair into cache
+                              (if (= pos (vector-length memo)) ; check for last position in cache
+                                  (set! pos 0) ; set current position counter to 0
+                                  (set! pos (+ pos 1))) ; increment pos counter
+                              new-ans) ; return ans
+                            #f)))))]) ; not found
+    f))
--- assignment-4/main.rkt
+++ assignment-4/main.rkt
@@ -1,68 +1,203 @@
 #lang racket
-;;
-;;
-(require "functions.rkt")
 
+(require "functions.rkt")
 (require rackunit)
 
-;; test create-stream
+
+; ---------- create-stream tests ---------- ;
+; dmg test
 (create-stream squares using (lambda (x) (* x x)) 
-          starting at  5
-          with increment 2)
+               starting at 5
+               with increment 2)
+
+; turtle test
+(define turtle-stream-counter-start 0)
+(define turtle-stream-counter-inc 0)
+(create-stream turtle-squares using (lambda (x) (* x x)) 
+               starting at (begin (set! turtle-stream-counter-start (+ turtle-stream-counter-start 1)) 5)
+               with increment (begin (set! turtle-stream-counter-inc (+ turtle-stream-counter-inc 1)) 2))
+
+; (vicky test)
+(create-stream div2 using (lambda (x) (/ x 2)) starting at 4 with increment -2)
+
 
 (define tests
   (test-suite
    "Sample tests for Assignment 4"
 
-
+   ; ---------- add-pointwise tests ---------- ;
+   ; dmg test
    (check-equal? (add-pointwise '(1 2 3) '(4 2))
                  (list 5 4 3)
                  "add-pointwise test")
 
+   ; troy tests
+   (check-equal? (add-pointwise '(1 2 3) null) (list 1 2 3) "add-pointwise test")
+   (check-equal? (add-pointwise null null) null "add-pointwise test")
+   (check-equal? (add-pointwise '(1) '(1 2 3 4 5 6 7)) (list 2 2 3 4 5 6 7) "add-pointwise test")
+   ;(check-equal? (add-pointwise 5 "test") (list 5 4 3) "add-pointwise test") this test tests for the error
+   
+   ; (epwr tests)
+   (check-equal? (add-pointwise '(1 2 3) '(4 2)) (list 5 4 3) "add-pointwise test")
+   (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(0 5 10 15 20)) (list 1 7 13 19 25 6 7 8 9 10) "add-pointwise test ep1")
+   (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-1 -2 -3 -4 -5 -6)) (list 0 0 0 0 0 0 7 8 9 10) "add-pointwise test ep2")
+   (check-equal? (add-pointwise '(1 2 3 4 5 6 7 8 9 10) '(-2 10 15 20)) (list -1 12 18 24 5 6 7 8 9 10) "add-pointwise test ep3")
+   (check-equal? (add-pointwise '(0 5 10 15 20) '(-1 -2 -3 -4 -5 -6)) (list -1 3 7 11 15 -6) "add-pointwise test ep4")
+   (check-equal? (add-pointwise '(0 5 10 15 20) '(-2 10 15 20)) (list -2 15 25 35 20) "add-pointwise test ep5")
+   (check-equal? (add-pointwise '(-1 -2 -3 -4 -5 -6) '(-2 10 15 20)) (list -3 8 12 16 -5 -6) "add-pointwise test ep6")
+   
+   ; (vicky tests)
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise "hi" '(1 2))) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise '() 1)) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise 1 2)) "illegal parameter" "add-pointwise test")
+
+   
+   ; ---------- add-pointwise-lists tests ---------- ;
+   ; dmg test
    (check-equal? (add-pointwise-lists '((1 1) (2 2 2 2) (3) ()))
                  '(6 3 2 2)
                  "add-pointwise-list test")
+   
+   ; (epwr tests)
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+   (check-equal? (add-pointwise-lists '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
+   ; (vicky tests)
+   (check-equal? (add-pointwise-lists '(())) '() "add-pointwise-list test")
+   (check-equal? (add-pointwise-lists '()) '() "add-pointwise-list test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists '((1 2) () 3))) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists '("hi" (1 2) (-1 -2)))) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists '("hi"))) "illegal parameter" "add-pointwise test")
+
+
+   ; ---------- add-pointwise-lists-2 tests ---------- ;
+   ; dmg test
    (check-equal? (add-pointwise-lists-2 '((1 1) (2 2 2 2) (3) ()))
                  '(6 3 2 2)
                  "add-pointwise-list test")
+   
+   ; (epwr tests) 
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6))) '(0 5 10 15 20 0 7 8 9 10) "add-pointwise-list test ep1")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (0 5 10 15 20) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-100 -200 300 400 600))) '(-102 -185 325 435 620 0 7 8 9 10) "add-pointwise-list test ep2")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6) (-1 -2 -3 -4 -5 -6))) '(-2 -4 -6 -8 -10 -12 7 8 9 10) "add-pointwise-list test ep3")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10))) '(1 2 3 4 5 6 7 8 9 10) "add-pointwise-list test ep4")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-2 10 15 20) (-100 -200 300 400 600))) '(-101 -188 318 424 605 6 7 8 9 10) "add-pointwise-list test ep5")
+   (check-equal? (add-pointwise-lists-2 '((1 2 3 4 5 6 7 8 9 10) (-1 -2 -3 -4 -5 -6) (-2 10 15 20) (-2 10 15 20))) '(-4 20 30 40 0 0 7 8 9 10) "add-pointwise-list test ep6")
 
+   ; (vicky tests)
+   (check-equal? (add-pointwise-lists-2 '(())) '() "add-pointwise-list test")
+   (check-equal? (add-pointwise-lists-2 '()) '() "add-pointwise-list test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists-2 '((1 2) () 3))) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists-2 '("hi" (1 2) (-1 -2)))) "illegal parameter" "add-pointwise test")
+   (check-equal? (with-handlers ([exn:fail? (lambda (exn) (exn-message exn))]) (add-pointwise-lists-2 '("hi"))) "illegal parameter" "add-pointwise test")
+
+
+   ; ---------- stream-for-n-steps tests ---------- ;
+   ; dmg test
    (check-equal? (stream-for-n-steps nat-num-stream 10)
                  '(0 1 2 3 4 5 6 7 8 9)
                  "stream-for-n-steps test")
+   
+   ; (epwr tests)
+   (check-equal? (stream-for-n-steps nat-num-stream 15) '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14) "nat-num-stream test ep1")
+   (check-equal? (stream-for-n-steps nat-num-stream 20) '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19) "nat-num-stream test ep2")
+   (check-equal? (stream-for-n-steps nat-num-stream 25) '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24) "nat-num-stream test ep3")
+   (check-equal? (stream-for-n-steps nat-num-stream 30) '(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29) "nat-num-stream test ep4")
 
+   ; (vicky tests)
+   (check-equal? (stream-for-n-steps nat-num-stream 1) '(0) "nat-num-stream test")
+   (check-equal? (stream-for-n-steps nat-num-stream 0) '() "nat-num-stream test")
+
+  
+   ; ---------- fibo-stream tests ---------- ;
+   ; dmg test
    (check-equal? (stream-for-n-steps fibo-stream 10)
                  '(0 1 1 2 3 5 8 13 21 34)
                  "fibo-stream test")
+   
+   ; (epwr tests)
+   (check-equal? (stream-for-n-steps fibo-stream 15) '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377) "fibo-stream test ep2")
+   (check-equal? (stream-for-n-steps fibo-stream 20) '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181) "fibo-stream test ep2")
+   (check-equal? (stream-for-n-steps fibo-stream 25) '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368) "fibo-stream test ep3")
+   (check-equal? (stream-for-n-steps fibo-stream 30) '(0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229) "fibo-stream test ep4")
 
+   ; (vicky tests)
+   (check-equal? (stream-for-n-steps fibo-stream 2) '(0 1) "fibo-stream test")
+   (check-equal? (stream-for-n-steps fibo-stream 1) '(0) "fibo-stream test")
+   (check-equal? (stream-for-n-steps fibo-stream 0) '() "fibo-stream test")
+   
+  
+   ; ---------- filter-stream tests ---------- ;
+   ; dmg test
    (check-equal? (stream-for-n-steps 
                   (filter-stream (lambda (i) (> i 5)) nat-num-stream) 5)
                  '(6 7 8 9 10)
                  "filter stream test")
-
+   
+   ; (epwr tests)
+   (check-equal? (stream-for-n-steps (filter-stream (lambda (i) (integer? (/ i 3))) nat-num-stream) 5) '(0 3 6 9 12) "filter nat-num-stream test")
+   (check-equal? (stream-for-n-steps (filter-stream (lambda (i) (> i 5)) fibo-stream) 15) '(8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765) "filter fibo-stream test ep1")
+   (check-equal? (stream-for-n-steps (filter-stream (lambda (i) (> i 10)) palyndromic-numbers) 11) '(11 22 33 44 55 66 77 88 99 101 111) "filter palyndromic-numbers test ep1")    
+   
+  
+   ; ---------- palyndromic-numbers tests ---------- ;
+   ; dmg test
    (check-equal? (stream-for-n-steps palyndromic-numbers 20)
                  '(0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101)
                  "palyndromic-numbers test")
+   
+   ; (epwr tests)
+   (check-equal? (stream-for-n-steps palyndromic-numbers 15) '(0 1 2 3 4 5 6 7 8 9 11 22 33 44 55) "palyndromic-numbers test ep1")
 
+
+   ; ---------- create-stream tests ---------- ;
+   ; dmg test
    (check-equal? (stream-for-n-steps squares 5)
                  '(25 49 81 121 169)
                  "stream defined using a macro. only tests is return value")
+   
+   ; (turtle)
+   (check-equal? (begin (turtle-squares) turtle-stream-counter-start) 1 "start should be called only once")
+   (set! turtle-stream-counter-start 0)
+   (check-equal? (begin (stream-for-n-steps turtle-squares 5) turtle-stream-counter-inc) 4 "should inc 4 times")
+   (check-equal? turtle-stream-counter-start 1 "start should be called only once")
+   (set! turtle-stream-counter-start 0)
+   (set! turtle-stream-counter-inc 0)
+  
+   ; (vicky - create-stream test)
+   (check-equal? (stream-for-n-steps div2 6) '(2 1 0 -1 -2 -3) "stream defined using a macro. only tests is return value")
+   
 
+   ; ---------- vector-assoc tests ---------- ;
+   ; dmg test
    (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1)))
                  (cons 4 1)
                  "vector-assoc test")
+   
+   ;(Greay tests)
+   (check-equal? (vector-assoc 4 (vector #f #f #f #f #f #f)) #f "vector-assoc test")
 
+   ; (vicky tests)
+   (check-equal? (vector-assoc 5 (vector 5 (cons 2 1))) #f "vector-assoc test")
+   (check-equal? (vector-assoc -2 (vector (vector (cons -2 1)) (cons -2 1))) (cons -2 1) "vector-assoc test")
+   (check-equal? (vector-assoc 2 (vector (vector (cons 2 1)) (cons 4 1))) #f "vector-assoc test")
+   (check-equal? (vector-assoc 0 (vector)) #f "vector-assoc test")
+
+
+   ; ---------- cached-assoc tests ---------- ;
+   ; dmg test
    ;; note that the following test tests functionality but not performance
    (check-equal?
     (let
         [(cache (cached-assoc  (list (cons 1 2) (cons 3 4)) 3) )]
-           (cache 3))
+      (cache 3))
     (cons 3 4)
     "cached-assoc test")
-
-   
-   ))
+  ))
 
 (require rackunit/text-ui)
-;; runs the test
-(run-tests tests)
+(run-tests tests) ;; runs the test
